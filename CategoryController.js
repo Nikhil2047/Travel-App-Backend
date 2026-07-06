755737494408de9dd38899567f00877401d99032
Nikhil Achari <nikhilacharikumta903@gmail.com>
2026-07-06 22:07:43 +0530

    initial commit

---
+++ CategoryController.js
@@ -0,0 +1,12 @@
+const Category = require("../model/category.model");
+
+const categoryHandler = async(req,res)=>{
+    try {
+        const categories = await Category.find({});
+        res.json(categories); 
+    } catch (error) {
+        res.status(404).json({msg:"Could not find categories"})
+    }
+}
+
+module.exports = categoryHandler;
